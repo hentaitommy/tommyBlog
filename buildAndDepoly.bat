3a5844af6a6c02a98c2ee55276a59658412a99cb
tommyjqliu <tommyjqliu@tencent.com>
2021-10-20 21:32:16 +0800

    部署脚本

---
+++ buildAndDepoly.bat
@@ -0,0 +1,19 @@
+if "%~1" == "github" goto success
+if "%~1" == "gitee" goto success
+goto error
+:success
+rem build
+cd public/%1
+rem rm -rf *
+cd ../../
+hugo -e %1
+rem depoly
+cd public/%1
+git add .
+git commit --allow-empty-message -m ''
+git push
+cd ../../
+goto end
+:error
+echo invalid environment argument
+:end
